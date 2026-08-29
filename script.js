4505bd22eaeafae0df38b88dc2146b067c91d23a
SoySamu123 <slglopezgarciaelproo@gmail.com>
2026-08-29 09:55:07 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 function abrir(opcion) {
-    alert("PM funciona: " + opcion);
+
+    const contenido =
+        document.getElementById("contenido");
+
+    if (opcion === "👤 Mi Perfil") {
+
+        contenido.innerHTML = `
+            <div class="perfil">
+
+                <h2>👤 MI PERFIL</h2>
+
+                <div class="avatar">⚽</div>
+
+                <h3>Nombre del jugador</h3>
+
+                <input
+                    id="nombreJugador"
+                    type="text"
+                    placeholder="Escribe tu nombre"
+                >
+
+                <h3>⚽ Posición</h3>
+
+                <select id="posicionJugador">
+                    <option>Centrocampista</option>
+                    <option>Delantero</option>
+                    <option>Extremo derecho</option>
+                    <option>Extremo izquierdo</option>
+                    <option>Defensa</option>
+                    <option>Portero</option>
+                </select>
+
+                <div class="general">
+
+                    ⭐ GENERAL
+
+                    <strong id="generalJugador">
+                        0
+                    </strong>
+
+                </div>
+
+            </div>
+        `;
+
+    } else {
+
+        contenido.innerHTML = `
+            <h2>${opcion}</h2>
+            <p>🚧 Próximamente...</p>
+        `;
+
+    }
 }
